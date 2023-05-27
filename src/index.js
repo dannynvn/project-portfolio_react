fa71a0aa86d23ce3b8f034ca8a764275e6defacc
Danny Nguyen <dannynvn@gmail.com>
2023-05-27 14:31:47 -0700

    moved code from wrong js file to index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+import App from "./App";
+
+// entry point for the React app
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
