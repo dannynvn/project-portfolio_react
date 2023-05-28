659393e386d050a3129491beeb89c521b27c3f9e
Danny Nguyen <dannynvn@gmail.com>
2023-05-27 21:29:05 -0700

    added basic resume page

---
+++ src/components/Resume.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+// resume page with downloadable resume
+
+function Resume() {
+    return (
+        <div>
+            <h1>Resume</h1>
+            <p>
+                This is the resume page.
+            </p>
+        </div>
+    );
+};
+
+export default Resume;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-// The ReactDOM.render() method is used to render a React element into the DOM in the supplied container and return a reference to the component (or returns null for stateless components).
+// The ReactDOM.render() method is used to render a React element into the DOM 
+// in the supplied container and return a reference to the component 
+// (or returns null for stateless components).
 ReactDOM.render(
     <React.StrictMode>
         <App />
