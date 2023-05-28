7b702eadd9b8c67b62e832002093886393467984
Danny Nguyen <dannynvn@gmail.com>
2023-05-27 21:20:54 -0700

    added basic page for contact

---
+++ src/components/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+// contact me page with form
+
+function Contact() {
+    return (
+        <div>
+            <h1>Contact</h1>
+            <p>
+                This is the contact page.
+            </p>
+        </div>
+    );
+}
+
+export default Contact;
