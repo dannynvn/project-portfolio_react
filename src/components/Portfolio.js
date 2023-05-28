d96e4e766e95243f0eec995ce371c8bddb89e50c
Danny Nguyen <dannynvn@gmail.com>
2023-05-27 21:22:07 -0700

    added basic portfolio page

---
+++ src/components/Portfolio.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+// portfolio page with projects
+
+function Portfolio() {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <p>
+                This is the portfolio page.
+            </p>
+        </div>
+    );
+}
+
+export default Portfolio;
